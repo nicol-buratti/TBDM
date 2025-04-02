a775902b87740259180747e29705e00a25e59470
nicol-buratti <nickibrt8@gmail.com>
2025-04-02 10:28:34 +0200

    removing neomodel from webscraper

--- web-scraper/main.py
+++ web-scraper/main.py
@@ -4,16 +4,11 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
-from neo4j.exceptions import ServiceUnavailable
 from tqdm import tqdm
-from dotenv import load_dotenv
-
-from models.database import Neo4jDatabase
 from models.paper import Paper
 from models.volume import Volume
 from models.people import Person
 from scraper.scraper import Scraper
-from neomodel import db, config
 from datetime import datetime
 
 log_path = Path("logs")
@@ -27,10 +22,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
-
-load_dotenv()
-NEO4J_URI = os.getenv("NEO4J_URI")
-config.DATABASE_URL = NEO4J_URI
 
 
 def load_json_volume(file_path):
@@ -46,16 +37,10 @@ def load_json_volume(file_path):
         logging.error(f"Error loading {file_path}: {e}")
 
 
-def process_volume(json_file):
-    volume = load_json_volume(json_file)
-    Neo4jDatabase.create_volume(volume)
-    return volume
-
-
 def main():
     scraper = Scraper()
 
-    all_volumes = scraper.get_all_volumes()
+    all_volumes = scraper.get_volumes_to_scrape()
 
     volume_path = Path("./data/Volumes")
     os.makedirs(volume_path, exist_ok=True)
@@ -77,26 +62,6 @@ def main():
             with open(file_path, "w") as json_file:
                 json.dump(volume, json_file, indent=4)
 
-        # Save all volumes in the database
-        json_files = [file for file in volume_path.iterdir() if file.suffix == ".json"]
-
-        for _ in tqdm(
-            executor.map(process_volume, json_files),
-            total=len(json_files),
-            desc="Saving volumes",
-            unit="volume",
-        ):
-            pass
-
-def connect_to_database():
-    db.cypher_query("RETURN 1")  # Simple query to validate the connection
-
 
 if __name__ == "__main__":
-    logging.info("Connected to Neo4j successfully.")
-    db.cypher_query(
-        """MATCH (n)
-    DETACH DELETE n
-    """
-    )
     main()
